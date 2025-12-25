3b7517e322b3b8b37536f2f2bd8702edacbed1bc
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-12-25 19:04:18 +0100

    zmena na napocet nad n2k_load

--- R/02_druhy/22_n2k_obdobi.R
+++ R/02_druhy/22_n2k_obdobi.R
@@ -1,27 +1,19 @@
-#----------------------------------------------------------#
-# Nacteni temp dat ----
-#----------------------------------------------------------#
-n2k_druhy <- 
-  readr::read_csv(
-    "Data/Temp/n2k_druhy.csv"
-    )
-
 # hodnocene obdobi - chu - druh - lokalita - pole ----
-n2k_druhy_obdobi_lok <- n2k_druhy %>%
+n2k_druhy_obdobi_lok <- n2k_load %>%
   dplyr::group_by(kod_chu, DRUH, KOD_LOKAL, POLE) %>%
   dplyr::reframe(
     HODNOCENE_OBDOBI_OD = min(DATUM, na.rm = TRUE),
     HODNOCENE_OBDOBI_DO = max(DATUM, na.rm = TRUE)
   )
 # hodnocene obdobi - chu - druh - pole ----
-n2k_druhy_obdobi_pol <- n2k_druhy %>%
+n2k_druhy_obdobi_pol <- n2k_load %>%
   dplyr::group_by(kod_chu, DRUH, POLE) %>%
   dplyr::reframe(
     HODNOCENE_OBDOBI_OD = min(DATUM, na.rm = TRUE),
     HODNOCENE_OBDOBI_DO = max(DATUM, na.rm = TRUE)
   )
 # hodnocene obdobi - chu - druh ----
-n2k_druhy_obdobi_chu <- n2k_druhy %>%
+n2k_druhy_obdobi_chu <- n2k_load %>%
   dplyr::group_by(kod_chu, DRUH) %>%
   dplyr::reframe(
     HODNOCENE_OBDOBI_OD = min(DATUM, na.rm = TRUE),
