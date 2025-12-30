d33684f0210e8ef9b542de9cf78f0f4b128bad08
Jonas Gaigr <jonas.gaigr@gmail.com>
2025-12-30 10:58:55 +0100

    odstraneni suppressWarnings

--- R/02_druhy/21_n2k_druhy_akce.R
+++ R/02_druhy/21_n2k_druhy_akce.R
@@ -989,13 +989,14 @@ run_n2k_druhy_lim <- function(
     limity,
     current_year = 2025
 ) {
- 
+  
   #----------------------------------------------------------#
-  # Prevod na long format a napojeni na limity ----- 
+  # 1. Prevod na long format a napojeni na limity ----- 
   #----------------------------------------------------------#
+
   n2k_druhy_long <- n2k_druhy %>%
     dplyr::mutate(
-      across(
+      dplyr::across(
         .cols = ncol_orig:ncol(.),
         .fns = ~ as.character(.)
       )
@@ -1035,15 +1036,24 @@ run_n2k_druhy_lim <- function(
     ) 
   
   # ------------------------------------------#
-  # Porovnani s limity ----- 
+  # 2. Porovnani s limity ----- 
   # ------------------------------------------#
   
-  n2k_druhy_lim_pre <- 
-    n2k_druhy_long %>%
+  n2k_druhy_lim_pre <- n2k_druhy_long %>%
     dplyr::mutate(
-      HOD_IND_num = suppressWarnings(as.numeric(HOD_IND)), 
-      LIM_IND_num = suppressWarnings(as.numeric(LIM_IND))
+      # 1. Orizneme mezery
+      HOD_IND_trim = stringr::str_trim(HOD_IND),
+      LIM_IND_trim = stringr::str_trim(LIM_IND),
+      # 2. Regex detekce: Je to cislo? (Volitelne minus, cislice, volitelne tecka a cislice)
+      # Pokud data obsahuji desetinnou carku misto tecky, regex vrati FALSE a vznikne NA (coz je pozadovane)
+      is_num_hod = stringr::str_detect(HOD_IND_trim, "^-?\\d+(\\.\\d+)?$"),
+      is_num_lim = stringr::str_detect(LIM_IND_trim, "^-?\\d+(\\.\\d+)?$"),
+      # 3. Podmineny prevod
+      HOD_IND_num = dplyr::if_else(is_num_hod, as.numeric(HOD_IND_trim), NA_real_),
+      LIM_IND_num = dplyr::if_else(is_num_lim, as.numeric(LIM_IND_trim), NA_real_)
     ) %>%
+    # Odstraneni pomocnych sloupcu pro cistotu
+    dplyr::select(-c(HOD_IND_trim, LIM_IND_trim, is_num_hod, is_num_lim)) %>%
     dplyr::mutate(
       STAV_IND = dplyr::case_when(
         TYP_IND == "min" & HOD_IND_num < LIM_IND_num ~ 0,
@@ -1063,6 +1073,7 @@ run_n2k_druhy_lim <- function(
     dplyr::mutate(
       is_POP = stringr::str_starts(ID_IND, "POP_")
     ) %>%
+    # Agregace vice hodnot pro jeden indikator (min/max logika)
     dplyr::mutate(
       STAV_IND = dplyr::case_when(
         IND_GRP == "minmax" & is_POP ~ max(as.numeric(STAV_IND), na.rm = TRUE),
@@ -1078,6 +1089,7 @@ run_n2k_druhy_lim <- function(
         TRUE ~ STAV_IND
       )
     ) %>%
+    # Vyber nejlepsi varianty pro unikatnost v ramci nalezu
     dplyr::group_by(
       ID_ND_NALEZ, 
       ID_IND
@@ -1087,8 +1099,9 @@ run_n2k_druhy_lim <- function(
     dplyr::ungroup()
   
   # ------------------------------------------#
-  # Hodnoceni nalezu ----- 
+  # 3. Hodnoceni nalezu ----- 
   # ------------------------------------------#
+  
   n2k_druhy_lim <- n2k_druhy_lim_pre %>%
     dplyr::group_by(ID_ND_NALEZ) %>%
     dplyr::mutate(
@@ -1114,7 +1127,6 @@ run_n2k_druhy_lim <- function(
     dplyr::filter(is.na(ID_ND_NALEZ) == FALSE)
   
   return(n2k_druhy_lim)
-  
 }
 
 #----------------------------------------------------------#
