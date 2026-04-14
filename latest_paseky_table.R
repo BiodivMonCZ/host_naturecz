c48c21d5e9a992e884c6a4d44ed0f72e879fd1ef
zbubster <zbu.rataj@seznam.cz>
2026-04-14 16:33:43 +0200

    priprava funckce na zisk nejnovejsi kombinace pasek; potreba optimalizovat nacitani dat

---
+++ latest_paseky_table.R
@@ -0,0 +1,108 @@
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+# cesty k adresarum
+
+dir_vmb1_vmb0 <- "../host_data/VMB1_VMB0"
+dir_vmb1_vmb2 <- "../host_data/VMB1_VMB2"
+dir_vmb2_vmb0 <- "../host_data/VMB2_VMB0"
+
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+# predmety ochrany
+
+protected_habitats <- sites_habitats |>
+  dplyr::transmute(
+    sitecode = base::as.character(site_code),
+    habitat = base::as.character(feature_code)
+  ) |>
+  dplyr::distinct()
+
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+# FUN
+# nacteni vsech GPKG ve slozce, ocisteni sloupcu
+
+read_paseky_simple <- function(dir_path, pair_name) {
+  
+  files <- fs::dir_ls(
+    path = dir_path,
+    glob = "*.gpkg",
+    recurse = FALSE
+  ) |>
+    base::as.character()
+  
+  purrr::map_dfr(files, function(f) {
+    
+    sf::st_read(f, quiet = TRUE) |>
+      sf::st_drop_geometry() |>
+      janitor::clean_names() |>
+      dplyr::mutate(
+        pair = pair_name,
+        source_file = base::basename(f),
+        sitecode = base::as.character(sitecode),
+        habitat = base::as.character(habitat),
+        region_id = base::as.character(region_id_x),
+        datum_new = base::as.Date(datum_x),
+        datum_old = base::as.Date(datum_x_1)
+      ) |>
+      dplyr::select(
+        sitecode,
+        habitat,
+        region_id,
+        pair,
+        source_file,
+        datum_new,
+        datum_old
+      )
+    
+  })
+}
+
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+# load data
+
+system.time(
+  x <- read_paseky_simple(dir_vmb1_vmb0, "VMB1_VMB0")
+)
+
+all_paseky <- dplyr::bind_rows(
+  read_paseky_simple(dir_vmb1_vmb0, "VMB1_VMB0"),
+  read_paseky_simple(dir_vmb1_vmb2, "VMB1_VMB2"),
+  read_paseky_simple(dir_vmb2_vmb0, "VMB2_VMB0")
+)
+
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+# prune, nechat jen predmety ochrany
+
+all_paseky_po <- all_paseky |>
+  dplyr::inner_join(
+    protected_habitats,
+    by = c("sitecode", "habitat")
+  )
+
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+# sumarizace
+# pro kombinaci EVL-habitat(PO)-okrsek-casovy prurez VMBx-VMBx vezmi stare a nove datum
+
+pair_summary <- all_paseky_po |>
+  dplyr::group_by(sitecode, habitat, region_id, pair) |>
+  dplyr::summarise(
+    datum_new = base::max(datum_new, na.rm = TRUE),
+    datum_old = base::max(datum_old, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
+# vyber nenjnovejsi kombinace
+# napric casovymi prurezy VMBx-VMBx pro dane EVL-habitat-okrsek serad sestupne
+# data (terminy), nasledne vezmi jenom prvni radek
+
+latest_choice <- pair_summary |>
+  dplyr::group_by(sitecode, habitat, region_id) |>
+  dplyr::arrange(
+    dplyr::desc(datum_new),
+    dplyr::desc(datum_old),
+    .by_group = TRUE
+  ) |>
+  dplyr::slice(1) |>
+  dplyr::ungroup()
+
+latest_choice
+# - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - # - #
