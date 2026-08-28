178002df66d5066772a8c9549d6c4c286ea1475d
Jonas Gaigr <jonas.gaigr@gmail.com>
2026-08-28 22:22:18 +0200

    automaticke nacitani config

--- R/02_druhy/20_n2k_druhy_run.R
+++ R/02_druhy/20_n2k_druhy_run.R
@@ -6,13 +6,34 @@
 # vyhodnoceni: 21 akce, 22 obdobi, 23 posledni nalez, 24 lokality,
 # 25 uzemi, 27 zapis/export).
 #
-# Predpoklada jiz spusteny R/00_config/00_n2k_config.R, ktery pripravuje
-# vstupni objekty (n2k_load, sites_subjects, limity, evl, rp_code,
-# n2k_oop, biotop_evd, indikatory_id, cis_ryby_delky, cis_pocet_kat).
+# Stoji na R/00_config/00_n2k_config.R, ktery pripravuje vstupni objekty
+# (n2k_load, sites_subjects, limity, evl, rp_code, n2k_oop, biotop_evd,
+# indikatory_id, cis_ryby_delky, cis_pocet_kat). Pokud config jeste nebezel,
+# spusti se automaticky; uz nactena data se znovu necetou.
 #----------------------------------------------------------#
 
+config_script <- "R/00_config/00_n2k_config.R"
+
 if (!exists("n2k_load")) {
-  stop("Objekt 'n2k_load' neexistuje - nejprve spustte R/00_config/00_n2k_config.R")
+
+  if (!file.exists(config_script)) {
+    stop(
+      "Objekt 'n2k_load' neexistuje a config '", config_script, "' nebyl nalezen",
+      " - spustte skript z korene repozitare, nebo nejprve nactete config rucne."
+    )
+  }
+
+  message("Objekt 'n2k_load' neexistuje - spoustim ", config_script)
+  source(config_script, encoding = "UTF-8")
+
+  # Config mohl probehnout, aniz by vstupni data vznikla (napr. chybejici
+  # export z NDOP) - bez 'n2k_load' by kaskada spadla az uvnitr 21_1.
+  if (!exists("n2k_load")) {
+    stop(
+      "Config '", config_script, "' probehl, ale objekt 'n2k_load' stale",
+      " neexistuje - zkontrolujte nacteni zdrojovych dat z NDOP."
+    )
+  }
 }
 
 #----------------------------------------------------------#
